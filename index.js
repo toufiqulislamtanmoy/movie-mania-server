c9bf0c46e0855318c9df8e0eee071d0646bae693
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-08-12 11:08:15 +0600

    add ssl payment integration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-
+const SSLCommerzPayment = require('sslcommerz-lts');
 const port = process.env.PORT || 5000
 
 app.use(cors());
@@ -32,6 +32,10 @@ const verifyJWT = (req, res, next) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zvd8xno.mongodb.net/?retryWrites=true&w=majority`;
 
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWORD;
+const is_live = false //true for live, false for sandbox
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -44,11 +48,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    client.connect();
     // Send a ping to confirm a successful connection
 
     const moviesCollections = client.db("movieMonster").collection("movies");
     const usersCollections = client.db("movieMonster").collection("users");
+    const pricingCollections = client.db("movieMonster").collection("pricing");
+    const orderCollections = client.db("movieMonster").collection("order");
 
     /********JWT api call*******/
     app.post('/jwt', (req, res) => {
@@ -95,20 +101,20 @@ async function run() {
 
     /********Find The user Role*******/
 
-    app.get('/role/:email',async (req,res) =>{
+    app.get('/role/:email', async (req, res) => {
       const email = req.params.email;
       // console.log(email);
-      const query = {email:email}
+      const query = { email: email }
       const options = {
-        projection: {role: 1,credit:1},
+        projection: { role: 1, credit: 1 },
       };
       const result = await usersCollections.findOne(query, options);
       res.send(result);
-      
+
     })
 
     // add a movie
-    app.post('/addMovie',verifyJWT,verifyAdmin, async(req,res) => {
+    app.post('/addMovie', verifyJWT, verifyAdmin, async (req, res) => {
       const movieDetails = req.body;
       console.log(movieDetails);
       const result = await moviesCollections.insertOne(movieDetails);
@@ -116,9 +122,9 @@ async function run() {
     })
 
     // decrement credit when user click download button
-    app.patch('/user/credit/:email',async(req,res) =>{
+    app.patch('/user/credit/:email', async (req, res) => {
       const email = req.params.email;
-      const query = {email: email};
+      const query = { email: email };
       const findUser = await usersCollections.findOne(query);
       if (findUser && findUser.credit > 0) {
         const updatedUserCredit = await usersCollections.updateOne(query, { $inc: { credit: -1 } });
@@ -126,6 +132,99 @@ async function run() {
       } else {
         res.send({ message: "Insufficient credits" });
       }
+    })
+
+    // pricing 
+    app.get("/plans", async (req, res) => {
+      const result = await pricingCollections.find().toArray();
+      res.send(result);
+    })
+
+    app.post("/buycredit", async(req,res) =>{
+      const purchesInfo = req.body;
+      const query = { _id: new ObjectId(purchesInfo.plansid)};
+      const plan = await pricingCollections.findOne(query);
+      // console.log(purchesInfo);
+      // console.log(plan);
+      const transactionId= new ObjectId().toString();
+      // sslcommerze
+      const data = {
+        total_amount:plan?.price,
+        currency: 'BDT',
+        tran_id: transactionId, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${transactionId}`,
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: plan?.name,
+        product_category: 'Credit',
+        product_profile: 'general',
+        cus_name: purchesInfo?.displayName,
+        cus_email: purchesInfo?.email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+    // console.log(data);
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+    sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.send({url:GatewayPageURL})
+        const order = {
+          plansid: purchesInfo.plansid,
+          customerName: purchesInfo.displayName,
+          customerEmail: purchesInfo.email,
+          customerId: purchesInfo.uid,
+          planName: plan.name,
+          price: plan.price,
+          description: plan.description,
+          paymentStatus: false,
+          transactionId,
+          totalCredit: plan.creditAmount + parseInt(plan.bonusCredits)
+        };
+        // console.log("Order: ",order);
+        const insertOrder = orderCollections.insertOne(order);
+    });
+      //sslcommerze end
+      app.post("/payment/success/:transID", async(req,res)=>{
+        
+        const findOrder = await orderCollections.findOne({ transactionId: req.params.transID});
+        console.log(findOrder?.customerEmail);
+        const orderUpdateResult = await orderCollections.updateOne(
+          { transactionId: req.params.transID },
+          { $set: { paymentStatus: true } }
+          );
+          console.log('update payment status');
+
+          const userUpdateResult = await usersCollections.updateOne(
+            { email: findOrder.customerEmail },
+            { $set: { credit: findOrder.totalCredit } }
+          );
+
+          if(userUpdateResult.modifiedCount>0){
+           res.redirect(`https://movie-monster-fa66c.web.app/paymentsuccess/${req.params.transID}`)
+          }
+      })
+    })
+
+    app.get('/order/:tranID', async(req,res)=>{
+      const transactionID = req.params.tranID;
+      const orderDetail = await orderCollections.findOne({transactionId:transactionID});
+      res.send(orderDetail);
     })
 
     client.db("admin").command({ ping: 1 });
